be4c26964e48cb9f3e9e39483eda7dc4b2762284
vivyvvv <1695147168@qq.com>
2025-03-04 14:29:23 +0800

    init

---
+++ Assets/Script/MapManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    private void Test(){
+        // DagreInputGraph dg = new DagreInputGraph();
+        // var nd1 = dg.AddNode();
+        // var nd2 = dg.AddNode();
+        // dg.AddEdge(nd1, nd2);
+        // dg.Layout(); 
+        // Console.WriteLine($"node1 : {nd1.X} {nd1.Y}");
+        // Console.WriteLine($"node2 : {nd2.X} {nd2.Y}");
+
+    }
+}
